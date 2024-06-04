396b0c0980f2e5ecec644156f2f61777741510c6
saliaida <154986231+saliaida@users.noreply.github.com>
2024-06-04 16:04:30 +0330

    Add files via upload

---
+++ myclass.py
@@ -0,0 +1,34 @@
+class Test:
+    def __init__(self):
+        self.a = 10
+        self._b = None
+        self.__c = 30
+        
+class Sub(Test):
+    def __init__(self):
+        Test.__init__(self)
+        # print(self.__c)
+    pass
+
+
+_x = 5
+__y = 6
+
+def _xx():
+    print('******')
+    
+def __yy():
+    print('++++++++')    
+
+# t1 = Test()
+# t1.a = 100
+# print(t1.a)
+# t1._b = 200
+# print(t1._b)
+# t1.__c = 300
+# # print(t1.__c)
+
+# s1 = Sub()
+# s1._b = 200
+# print(s1._b)
+# t1.a = 100
